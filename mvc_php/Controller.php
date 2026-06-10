331cc018918e266edfc0a80c4cc385a0e89777ef
Letícia Almeida <piresalmeidaleticia@gmail.com>
2026-06-10 16:50:59 +0000

    MVC2

--- mvc_php/Controller.php
+++ mvc_php/Controller.php
@@ -8,6 +8,21 @@ class Controller {
     }
 
     public function somar(){
-        $this->model->resultado = $this->model->numero1 + 
+        $this->model->resultado = $this->model->numero1 + $this->model->numero2;
     }
+
+    public function subtrair(){
+        $this->model->resultado = $this->model->numero1 - $this->model->numero2;
+    }
+
+    public function multiplicar(){
+        $this->model->resultado = $this->model->numero1 * $this->model->numero2;
+    }
+
+    public function dividir(){
+        $this->model->resultado = $this->model->numero1 / $this->model->numero2;
+    } else {
+        $this->model->reusltado = "Erro: divisão por zero!";
+    }
+}
 }
